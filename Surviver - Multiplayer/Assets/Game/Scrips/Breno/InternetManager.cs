4f9df7db4debd85ab2d2a59e65dff78e01f040cb
brenoia35 <57221730+brenoia35@users.noreply.github.com>
2020-03-01 19:43:43 -0300

    Preparo
    Preparação do projeto para personagem e movimentação de camera... Usando sistema complexo de camera e movimentação que será criado com o Photon.

--- Surviver - Multiplayer/Assets/Game/Scrips/Breno/InternetManager.cs
+++ Surviver - Multiplayer/Assets/Game/Scrips/Breno/InternetManager.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 namespace Breno.Systema {
-	public class InternetManager : MonoBehaviour
+	public class InternetManager : MonoBehaviourPunCallbacks
 	{
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
 
-		// Use this for initialization
-		void Start()
-		{
+        public override void OnConnected()
+        {
+            Debug.Log("Connected server...");
+        }
 
-		}
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Serve: " + PhotonNetwork.CloudRegion + " Ping: " + PhotonNetwork.GetPing());
+        }
 
-		// Update is called once per frame
-		void Update()
-		{
-
-		}
-	}
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Disconected because: " + cause);
+        }
+    }
 }
