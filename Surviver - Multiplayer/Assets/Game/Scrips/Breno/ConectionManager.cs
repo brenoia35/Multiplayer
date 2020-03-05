b3d5dc58d6888767e4d9b65780dc3db1d8deedb0
brenoia35 <57221730+brenoia35@users.noreply.github.com>
2020-03-04 21:32:35 -0300

    ReloadScript

---
+++ Surviver - Multiplayer/Assets/Game/Scrips/Breno/ConectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Breno.Systema {
+	public class ConectionManager : MonoBehaviourPunCallbacks
+	{
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnected()
+        {
+            Debug.Log("Connected server...");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Serve: " + PhotonNetwork.CloudRegion + " Ping: " + PhotonNetwork.GetPing());
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Disconected because: " + cause);
+        }
+    }
+}
